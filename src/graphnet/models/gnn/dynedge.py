c52795c4f9935d6c8b85e2a81b87c66aa8062d77
Rasmus Oersoe <rahn@outlook.dk>
2022-07-22 16:42:37 +0200

    jajaja

--- src/graphnet/models/gnn/dynedge.py
+++ src/graphnet/models/gnn/dynedge.py
@@ -160,6 +160,167 @@ class DynEdge(GNN):
         return x
 
 
+class DynEdge_State_Graph_Illustration(GNN):
+    def __init__(self, nb_inputs, save_path, layer_size_scale=4):
+        """DynEdge model.
+
+        Args:
+            nb_inputs (int): Number of input features.
+            nb_outputs (int): Number of output features.
+            layer_size_scale (int, optional): Integer that scales the size of
+                hidden layers. Defaults to 4.
+        """
+
+        # Architecture configuration
+        self._save_graphs = False
+        self._save_path = save_path
+        c = layer_size_scale
+        l1, l2, l3, l4, l5, l6 = (
+            nb_inputs,
+            c * 16 * 2,
+            c * 32 * 2,
+            c * 42 * 2,
+            c * 32 * 2,
+            c * 16 * 2,
+        )
+
+        # Base class constructor
+        super().__init__(nb_inputs, l6)
+
+        # Graph convolutional operations
+        features_subset = slice(0, 3)
+        nb_neighbors = 8
+
+        self.conv_add1 = DynEdgeConv(
+            torch.nn.Sequential(
+                torch.nn.Linear(l1 * 2, l2),
+                torch.nn.LeakyReLU(),
+                torch.nn.Linear(l2, l3),
+                torch.nn.LeakyReLU(),
+            ),
+            aggr="add",
+            nb_neighbors=nb_neighbors,
+            features_subset=features_subset,
+        )
+
+        self.conv_add2 = DynEdgeConv(
+            torch.nn.Sequential(
+                torch.nn.Linear(l3 * 2, l4),
+                torch.nn.LeakyReLU(),
+                torch.nn.Linear(l4, l3),
+                torch.nn.LeakyReLU(),
+            ),
+            aggr="add",
+            nb_neighbors=nb_neighbors,
+            features_subset=features_subset,
+        )
+
+        self.conv_add3 = DynEdgeConv(
+            torch.nn.Sequential(
+                torch.nn.Linear(l3 * 2, l4),
+                torch.nn.LeakyReLU(),
+                torch.nn.Linear(l4, l3),
+                torch.nn.LeakyReLU(),
+            ),
+            aggr="add",
+            nb_neighbors=nb_neighbors,
+            features_subset=features_subset,
+        )
+
+        self.conv_add4 = DynEdgeConv(
+            torch.nn.Sequential(
+                torch.nn.Linear(l3 * 2, l4),
+                torch.nn.LeakyReLU(),
+                torch.nn.Linear(l4, l3),
+                torch.nn.LeakyReLU(),
+            ),
+            aggr="add",
+            nb_neighbors=nb_neighbors,
+            features_subset=features_subset,
+        )
+
+        # Post-processing operations
+        self.nn1 = torch.nn.Linear(l3 * 4 + l1, l4)
+        self.nn2 = torch.nn.Linear(l4, l5)
+        self.nn3 = torch.nn.Linear(4 * l5 + 5, l6)
+        self.lrelu = torch.nn.LeakyReLU()
+
+    def forward(self, data: Data) -> Tensor:
+        """Model forward pass.
+
+        Args:
+            data (Data): Graph of input features.
+
+        Returns:
+            Tensor: Model output.
+        """
+
+        # Convenience variables
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        if self._save_graphs:
+            torch.save(x, self._save_path + "/input_x.pt")
+            torch.save(edge_index, self._save_path + "/input_edges.pt")
+
+        # Calculate homophily (scalar variables)
+        h_x, h_y, h_z, h_t = calculate_xyzt_homophily(x, edge_index, batch)
+
+        a, edge_index = self.conv_add1(x, edge_index, batch)
+        if self._save_graphs:
+            torch.save(a, self._save_path + "/state_1_x.pt")
+            torch.save(edge_index, self._save_path + "/state_1_edges.pt")
+        b, edge_index = self.conv_add2(a, edge_index, batch)
+        if self._save_graphs:
+            torch.save(b, self._save_path + "/state_2_x.pt")
+            torch.save(edge_index, self._save_path + "/state_2_edges.pt")
+        c, edge_index = self.conv_add3(b, edge_index, batch)
+        if self._save_graphs:
+            torch.save(c, self._save_path + "/state_3_x.pt")
+            torch.save(edge_index, self._save_path + "/state_3_edges.pt")
+        d, edge_index = self.conv_add4(c, edge_index, batch)
+        if self._save_graphs:
+            torch.save(d, self._save_path + "/state_4_x.pt")
+            torch.save(edge_index, self._save_path + "/state_4_edges.pt")
+
+        # Skip-cat
+        x = torch.cat((x, a, b, c, d), dim=1)
+
+        # Post-processing
+        x = self.nn1(x)
+        x = self.lrelu(x)
+        x = self.nn2(x)
+
+        # Aggregation across nodes
+        a, _ = scatter_max(x, batch, dim=0)
+        b, _ = scatter_min(x, batch, dim=0)
+        c = scatter_sum(x, batch, dim=0)
+        d = scatter_mean(x, batch, dim=0)
+
+        # Concatenate aggregations and scalar features
+        x = torch.cat(
+            (
+                a,
+                b,
+                c,
+                d,
+                h_t.reshape(-1, 1),
+                h_x.reshape(-1, 1),
+                h_y.reshape(-1, 1),
+                h_z.reshape(-1, 1),
+                data.n_pulses.reshape(-1, 1),
+            ),
+            dim=1,
+        )
+
+        # Read-out
+        x = self.lrelu(x)
+        x = self.nn3(x)
+
+        x = self.lrelu(x)
+
+        return x
+
+
 class DynEdge_V2(GNN):
     def __init__(self, nb_inputs, layer_size_scale=4):
         """DynEdge model.
