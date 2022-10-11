de70e2d7743156eb8e3651ab0977bd9e02a2999a
Rasmus Oersoe <rahn@outlook.dk>
2022-10-11 16:24:09 +0200

    added VonMisesFisher3DLoss

--- src/graphnet/components/loss_functions.py
+++ src/graphnet/components/loss_functions.py
@@ -327,21 +327,25 @@ class EuclideanDistanceLoss(LossFunction):
 
 
 class VonMisesFisher3DLoss(VonMisesFisherLoss):
+    """von Mises-Fisher loss function vectors in the 3D plane."""
+
     def _forward(self, prediction: Tensor, target: Tensor) -> Tensor:
+        """Calculates the von Mises-Fisher loss for a direction in the 3D plane.
+
+        Args:
+            prediction (Tensor): Output of the model. Must have shape [N, 4]
+                where columns 0, 1, 2 are predictions of `direction` and last column is an
+                estimate of `kappa`.
+            target (Tensor): Target tensor, extracted from graph object.
+        Returns:
+            loss (Tensor): Elementwise von Mises-Fisher loss terms. Shape [N,]
+        """
         target = target.reshape(-1, 3)
         # Check(s)
         assert prediction.dim() == 2 and prediction.size()[1] == 4
         assert target.dim() == 2
         assert prediction.size()[0] == target.size()[0]
 
-        # Formatting target
-
-        # Formatting prediction
-
         kappa = prediction[:, 3]
         p = kappa.unsqueeze(1) * prediction[:, [0, 1, 2]]
         return self._evaluate(p, target)
-        # k = prediction[:,3] # has been abs'ed by task
-        # cos_ø = prediction[:,0]*target[:,0] + prediction[:,1]*target[:,1] + prediction[:,2]*target[:,2] #unit vectors by task / dataset
-        # c_3 = k/(torch.sinh(k))
-        # return -torch.log(c_3) - k*cos_ø #torch.log is natural log
