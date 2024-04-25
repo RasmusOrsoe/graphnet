c791d2b885d58856a7b087f586a357fb4cb9ac93
Rasmus Ørsøe <rahn@outlook.dk>
2024-04-25 12:45:02 +0200

    add two filters

--- src/graphnet/data/extractors/prometheus/utilities.py
+++ src/graphnet/data/extractors/prometheus/utilities.py
@@ -1,1 +1,74 @@
 """A series of utility functions for extraction of data from Prometheus."""
+
+import pandas as pd
+from abc import abstractmethod
+
+from graphnet.utilities.logging import Logger
+
+
+class PrometheusFilter(Logger):
+    """Generic Filter Class for PrometheusReader."""
+
+    def __init__(self, filter_on: str) -> None:
+        """Instantiate filter.
+
+        Args:
+            filter_on: Name of field in file to run filter on.
+        """
+        # Member Variables
+        self._filter_on = filter_on
+        # Base class constructor
+        super().__init__(name=__name__, class_name=self.__class__.__name__)
+
+    @abstractmethod
+    def __call__(self, event: pd.DataFrame) -> bool:
+        """Return True if the event should be kept.
+
+        Return False i the event should be skipped.
+        """
+
+
+class EmptyFilter(PrometheusFilter):
+    """Skip empty events."""
+
+    def __call__(self, event: pd.DataFrame) -> bool:
+        """Skip empty events.
+
+        Args:
+            event: photons from event.
+
+        Returns:
+            True if event is not empty.
+        """
+        if len(event) > 1:
+            return True
+        else:
+            return False
+
+
+class MaxPhotonCount(PrometheusFilter):
+    """Skip events exceeding a specific number of photons."""
+
+    def __init__(self, filter_on: str, max_photons: int = 500000) -> None:
+        """Skip events exceeding a specific number of photons.
+
+        Args:
+            filter_on: Name of field in file to run filter on.
+            max_photons: Maximum number of photons. Defaults to 500000.
+        """
+        self._max_photons = max_photons
+        super().__init__(filter_on=filter_on)
+
+    def __call__(self, event: pd.DataFrame) -> bool:
+        """Skip events that exceed the maximum number of photons.
+
+        Args:
+            event: photons from event.
+
+        Returns:
+            True if event exceeds limit on photon count.
+        """
+        if len(event) > self._max_photons:
+            return True
+        else:
+            return False
